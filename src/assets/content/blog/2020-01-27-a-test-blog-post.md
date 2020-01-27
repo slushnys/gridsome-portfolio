72d69908baf7cb41b21be850b148662e163341e4
Zigmas Slusnys <zigmasslusnys@gmail.com>
2020-01-27 20:19:01 +0200

    fix(identation): config yaml had wrong identation configuration

---
+++ src/assets/content/blog/2020-01-27-a-test-blog-post.md
@@ -0,0 +1,8 @@
+---
+layout: blog
+title: "Goosebumps"
+date: 2018-11-20 23:42:14 -0700
+thumbnail: "/images/ghost.jpg"
+---
+
+This is the body content, where I write anything as long as I want, but it has to be in the markdown format, that's the only condition. 😉
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -47,7 +47,7 @@
           <div class="max-w-sm w-full lg:max-w-full lg:flex">
             <div
               class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
-              style="background-image: url('sendcloud-job.png')"
+              style="background-image: url('img/sendcloud-job.png')"
               title="Woman holding a mug"
             ></div>
             <div
