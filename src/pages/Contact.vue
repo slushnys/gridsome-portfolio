c1880a9b33cf52cfc7b89af53b95ae162ad85654
Zigmas Slusnys <zigmasslusnys@gmail.com>
2020-02-04 18:48:28 +0200

    Update for new deploy

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -2,7 +2,7 @@
   <Layout>
       <div class="flex">
           <!-- I'm excited to get in touch with you.  -->
-          <h3>I'll get back to you within 24 hours!</h3>
+          <h3>I'm glad you decided to contact me</h3>
       </div>
           <p>Please send me an email to <a href="mailto:zigmas@slusnys.com">zigmas@slusnys.com</a></p>
     <ContactForm class="mt-6"></ContactForm>
